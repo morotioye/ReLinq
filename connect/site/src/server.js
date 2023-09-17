3d7ec4859f04d07ad97f232b7842e683846c7f20
Moroti Oyeyemi <120543764+seeohh@users.noreply.github.com>
2023-09-17 08:47:21 -0400

    Update server.js

--- connect/site/src/server.js
+++ connect/site/src/server.js
@@ -3,8 +3,8 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const twilio = require('twilio');
 
-const accountSid = 'ACa126de581c6c0c0efa1b0a02ca93108e';
-const authToken = '5d2a7146508acf161edc8e97faafabb8';
+const accountSid = 'sid';
+const authToken = 'token';
 const client = new twilio(accountSid, authToken);
 
 const app = express();
